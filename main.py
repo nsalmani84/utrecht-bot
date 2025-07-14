8b3cca3326cb18e5fcc5dc587dcd5f1c235aadab
nsalmani84 <neda.salmani84@gmail.com>
2025-07-14 22:27:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import asyncio
 from telegram import Bot
 import os
 
-# دریافت توکن و chat_id از متغیرهای محیطی
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = int(os.getenv('CHAT_ID'))
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-try:
-    bot.send_message(chat_id=CHAT_ID, text="✅ تست موفق: بات تلگرام به درستی کار می‌کند!")
-    print("✅ پیام با موفقیت ارسال شد.")
-except Exception as e:
-    print("❌ خطا در ارسال پیام:", e)
+async def send_test_message():
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text="✅ تست موفق: بات تلگرام به درستی کار می‌کند!")
+        print("✅ پیام با موفقیت ارسال شد.")
+    except Exception as e:
+        print("❌ خطا در ارسال پیام:", e)
+
+if __name__ == "__main__":
+    asyncio.run(send_test_message())
